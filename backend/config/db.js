03af6bcfcb5ca6036fff851966a0ccb4ea7ad5a4
henrikpoa <henrik_poa@hotmail.com>
2024-10-17 16:01:22 -0300

    user and password mogodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS;
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ljrag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.7eqtz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("Conectou ao banco de dados!");
 
